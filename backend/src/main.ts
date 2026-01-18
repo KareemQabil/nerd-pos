a8c4e56c086d1e93cebe46611184653fb2a8a08f
KareemQabil <446109898@student.ksu.edu.sa>
2026-01-18 13:27:28 +0300

    fix(main): add ValidationPipe global registration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Validation - DTOs will now be validated
+  app.useGlobalPipes(createValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
+
